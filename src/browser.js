ac5b57f63f80b3b95eed219305adfb9d951b925d
Cain Atkinson <19270622+yellowsink@users.noreply.github.com>
2022-05-26 09:41:38 +0000

    i do love bundle size optimising

--- src/browser.js
+++ src/browser.js
@@ -1,3 +1,12 @@
 import cummunicate from ".";
 
 window.cummunicate = cummunicate;
+
+/*
+ * this makes the esm.sh bundle MUCH smaller LMAO
+ * because it just re-exports cummunicate as c
+ * (doesnt actually need an `as` because esbuild magic)
+ * as opposed to seeing no exports and pissing about trying
+ * to convert cjs / iife / whatever to esm
+ */
+export { cummunicate as c };
